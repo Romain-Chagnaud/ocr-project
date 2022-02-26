60e57965209e463fe037e8c40ef44516153682d6
retromodern <alex2002hiphop@gmail.com>
2022-02-26 18:36:33 +0100

    amélioration des résultats

--- ocr-png.py
+++ ocr-png.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy
+import os, shutil
 
 
 def read_image(path):
@@ -11,7 +12,7 @@ def read_images(n_max_images):
     nb = 0
     nb_range = 1
     for i in range(0,n_max_images):
-        # redimensionnement
+        # redimensionnement de l'image
         im = Image.open(f'data/baseProjetOCR/{nb}_{nb_range}.png')
         res = im.resize((200, 200))
         res.save(f'data/baseProjetOCR/test/{nb}_{nb_range}.png')
@@ -39,7 +40,7 @@ def extract_features(X):
 
 
 def dist(x, y):
-    # euclidean distance between vectors x and y
+    # distance euclidienne entre les vecteurs x et y
     return sum(
         [
             (bytes_to_int(x_i) - bytes_to_int(y_i)) ** 2
@@ -79,13 +80,33 @@ def knn(X_test, y_test, k):
     print()
     return y_pred
 
+def delete_test_folder():
+    folder = 'data/baseProjetOCR/test/'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Echec de la suppression %s : %s' % (file_path, e))
+    os.rmdir(f'data/baseProjetOCR/test/')
+    pass
 
 def main():
+    # options du test
     n_test = 100
     k = 7
     print(f'n_test: {n_test}')
     print(f'k: {k}')
 
+    # création du dossier de test
+    if os.path.exists(f'data/baseProjetOCR/test/'):
+        delete_test_folder()
+    os.mkdir(f'data/baseProjetOCR/test/')
+
+    # on prépare les images
     X_test = read_images(n_test)
     y_test = [0,0,0,0,0,0,0,0,0,0,
               1,1,1,1,1,1,1,1,1,1,
@@ -97,19 +118,24 @@ def main():
               7,7,7,7,7,7,7,7,7,7,
               8,8,8,8,8,8,8,8,8,8,
               9,9,9,9,9,9,9,9,9,9]
-
     X_test = extract_features(X_test)
 
+    # on effectue le réseau de neuronnes
     y_pred = knn(X_test, y_test, k)
 
+    # on calcule la précision du résultat
     accuracy = sum([
         int(y_pred_i == y_test_i)
         for y_pred_i, y_test_i
         in zip(y_pred, y_test)
     ]) / len(y_test)
 
+    # on affiche le résultat
     print(f'Predicted labels: {y_pred}')
     print(f'Accuracy: {accuracy * 100}%')
+
+    # suppression du dossier de test
+    delete_test_folder()
 
 
 if __name__ == '__main__':
